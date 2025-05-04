bf7de9a63639dd20741957945b04f94f8b6f7054
Maores <maorx40@gmail.com>
2025-05-04 18:41:03 +0300

    GUI Implementation && Fixes
    -Added GUI interface
    -Changed MySql password(change to your own after pulling)
    -Added an cursor hovering method button

---
+++ BParkApplication/src/client/GUIParkingClient.java
@@ -0,0 +1,36 @@
+
+package client;
+
+import common.ChatIF;
+
+public class GUIParkingClient implements ChatIF {
+
+    private ChatClient client;
+    private ParkingSystemGUI gui;
+
+    public GUIParkingClient(String host, int port, ParkingSystemGUI gui) {
+        this.gui = gui;
+        try {
+            client = new ChatClient(host, port, this);
+        } catch (Exception e) {
+            display("Error: Can't setup connection! Terminating client.");
+            System.exit(1);
+        }
+    }
+
+    public void sendMessage(String msg) {
+        client.handleMessageFromClientUI(msg);
+    }
+
+    public void display(String message) {
+        gui.displayMessage(message);
+    }
+
+    public String getLastServerResponse() {
+        return client.getLastServerResponse();
+    }
+
+    public void quit() {
+        client.quit();
+    }
+}
