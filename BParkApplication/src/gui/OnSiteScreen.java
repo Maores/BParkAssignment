b35648686d6bab1aeec34c5dca82314f70128a90
rafi145 <rafi12az@gmail.com>
2025-07-07 16:42:21 +0300

    minor minor

--- BParkApplication/src/gui/OnSiteScreen.java
+++ BParkApplication/src/gui/OnSiteScreen.java
@@ -3,11 +3,17 @@ package gui;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class OnSiteScreen {
@@ -30,7 +36,20 @@ public class OnSiteScreen {
     
     @FXML
     void Delivery(ActionEvent event) {
+    	ShowCodeScreen();
     	Terminal.appendText("Car is about to be taken by the lift, dont step over the line!\n");
+    }
+    void ShowCodeScreen() {
+    	Stage checkCode = new Stage();
+    	TextField code = new TextField();
+    	Button codeBtn = new Button("check");
+		VBox root = new VBox(new Label("Enter confirmation code:"),code,codeBtn);
+		root.setAlignment(Pos.CENTER);
+		checkCode.setScene(new Scene(root,300, 100));
+		checkCode.setAlwaysOnTop(true);
+		checkCode.setResizable(false);
+		checkCode.setTitle("Enter Code");
+		checkCode.show();
     }
 
     @FXML
